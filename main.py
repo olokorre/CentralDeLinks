58e5a48ee74e8c4c43e82cb9b618892c8f2e8762
olokorre <andreiruansteffens@gmail.com>
2019-12-03 23:49:57 -0300

    new style!

--- main.py
+++ main.py
@@ -3,34 +3,31 @@ import mysql.connector
 
 app = Flask(__name__)
 
-mydb = mysql.connector.connect(
-  user="olokorre",
-  passwd="Linux@290"
-)
+mydb = mysql.connector.connect(user="olokorre", passwd="Linux@290")
+mycursor = mydb.cursor()
+mycursor.execute("use Links")
 
 @app.route('/')
 def index():
-    nome = []
-    link = []
-    mycursor = mydb.cursor()
-    mycursor.execute("use Links")
-    mycursor.execute("select * from links")
-    for i in mycursor:
-        nome.append(i[1])
-        link.append(i[2])
-    return render_template('index.html', nome = nome, link = link, tam = len(nome))
+	nome = []
+	link = []
+	mycursor.execute("select * from links")
+	for i in mycursor:
+		nome.append(i[1])
+		link.append(i[2])
+	return render_template('index.html', nome = nome, link = link, tam = len(nome))
 
 @app.route('/js/<path:path>')
 def send_js(path):
-    return send_from_directory('static/js', path)
+	return send_from_directory('static/js', path)
 
 @app.route('/css/<path:path>')
 def send_css(path):
-    return send_from_directory('static/css', path)
+	return send_from_directory('static/css', path)
 
 @app.route('/image/<path:path>')
 def send_image(path):
-    return send_from_directory('static/image', path)
+	return send_from_directory('static/image', path)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+	app.run(debug=True, host='0.0.0.0', port=5000)
--- static/js/script.js
+++ static/js/script.js
@@ -1,23 +1,28 @@
-$(document).ready(() => { $("body").fadeIn("slow"); });
+$(document).ready(() => { 
+	setTimeout(() => { $("#barra").fadeIn("slow"); }, 1200);
+	setTimeout(() => { $("#mostrar").fadeIn("slow"); }, 2300);
+	$(".menu").addClass("transicao");
+});
 
 function MostrarLinks() {
-    $(".menu").fadeOut("slow");
-    setTimeout(() => { $("table").fadeIn("slow"); $("#Voltar").fadeIn("slow"); }, 1200);
+	$(".menu").fadeOut("slow").removeClass("transicao");
+	setTimeout(() => { $("table").fadeIn("slow"); $("#Voltar").fadeIn("slow"); }, 1200);
 }
 
 function AddLinks() {
-    $(".menu").fadeOut("slow");
-    setTimeout(() => { $("#import").fadeIn("slow"); $("#Voltar").fadeIn("slow"); }, 1200);
+	$(".menu").fadeOut("slow").removeClass("transicao");
+	setTimeout(() => { $("#import").fadeIn("slow"); $("#Voltar").fadeIn("slow"); }, 1200);
 }
 
 function Voltar() {
-    $("table").fadeOut("slow");
-    $("#import").fadeOut("slow");
-    $("#Voltar").fadeOut("slow");
-    setTimeout(() => { $(".menu").fadeIn("slow") }, 1200);
+	$("table").fadeOut("slow");
+	$("#import").fadeOut("slow");
+	$("#Voltar").fadeOut("slow");
+	setTimeout(() => { $(".menu").fadeIn("slow"); }, 1200);
+	setTimeout(() => { $(".menu").addClass("transicao"); }, 1500);
 }
 
 function Enviar() {
-    var nome = document.getElementById("#nome").value;
-    console.log(nome);
+	var nome = document.getElementById("#nome").value;
+	console.log(nome);
 }
