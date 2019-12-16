2884b4c26dc5b3fdfc94f70410388f0e95ae59f2
olokorre <andreiruansteffens@gmail.com>
2019-12-16 16:50:51 -0300

    só vai

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, send_from_directory, request, redirect
 import mysql.connector
+from datetime import datetime
 
 app = Flask(__name__)
-
-mydb = mysql.connector.connect(user = "olokorre", passwd = "Linux@290")
-mycursor = mydb.cursor()
-mycursor.execute("use Links")
 
 @app.route('/', methods = ('GET', 'POST'))
 def index():
 	nome = []
 	link = []
+	mydb = mysql.connector.connect(user = "olokorre", passwd = "Linux@290")
+	mycursor = mydb.cursor()
+	mycursor.execute("use Links")
 	mycursor.execute("select * from links")
 	for i in mycursor:
 		nome.append(i[1])
@@ -21,7 +21,11 @@ def index():
 		mycursor.execute('insert into links (nome, link) value ("%s", "%s")' % (n, l))
 		mydb.commit()
 		return redirect('/')
-	return render_template('index.html', nome = nome, link = link, tam = len(nome))
+	now = datetime.now()
+	if now.hour >= 6 and now.hour < 12: dar = "Bom Dia!"
+	elif now.hour >= 12 and now.hour < 19: dar = "Boa Tarde!"
+	else: dar = "Boa Noite!"
+	return render_template('index.html', dar = dar, nome = nome, link = link, tam = len(nome))
 
 @app.route('/js/<path:path>')
 def send_js(path):
--- static/js/script.js
+++ static/js/script.js
@@ -2,11 +2,18 @@ $(document).ready(() => {
 	setTimeout(() => { $("#barra").fadeIn("slow"); }, 1200);
 	setTimeout(() => { $("#mostrar").fadeIn("slow"); }, 2300);
 	$(".menu").addClass("transicao");
+	// $(".tabela").css("width", (window.screen.availWidth - 10) + "px");
+	// $(".tabela").css("height", "400px");
+	$(".tabela").css("overflow", "auto");
 });
 
 function MostrarLinks() {
+	// var tamTela = ;
+	// var tamTela = $(document).height();
+	$(".tabela").css("height", (window.screen.availHeight / 2) + "px");
 	$(".menu").fadeOut("slow").removeClass("transicao");
-	setTimeout(() => { $("table").fadeIn("slow"); $("#Voltar").fadeIn("slow"); }, 1200);
+	setTimeout(() => { $(".tabela").fadeIn("slow"); $("#Voltar").fadeIn("slow"); }, 1200);
+	setTimeout(() => { $("td").addClass("transicao"); }, 1500);
 }
 
 function AddLinks() {
@@ -15,14 +22,15 @@ function AddLinks() {
 }
 
 function Voltar() {
-	$("table").fadeOut("slow");
+	$("td").removeClass("transicao");
+	$(".tabela").fadeOut("slow");
 	$("#import").fadeOut("slow");
 	$("#Voltar").fadeOut("slow");
 	setTimeout(() => { $(".menu").fadeIn("slow"); }, 1200);
 	setTimeout(() => { $(".menu").addClass("transicao"); }, 1500);
 }
 
-function Enviar() {
-	var nome = document.getElementById("#nome").value;
-	console.log(nome);
+function Limpar(id) {
+	document.getElementById(id).value = "";
+	// alert("Maluco");
 }
