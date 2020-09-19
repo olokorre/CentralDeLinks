d17244f9e894c32c60348cc9dc3ec8055f230933
olokorre <andreiruansteffens@gmail.com>
2020-09-19 12:40:16 -0300

    erro bobo

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 class Saudacao(object):
 	def dar(self):
 		now = datetime.now() # consulta a hora do sistema operacional do servidor
-		if now.hour >= 6 and now.hour < 12: dar = "bom fia!"
+		if now.hour >= 6 and now.hour < 12: dar = "bom dia!"
 		elif now.hour >= 12 and now.hour < 19: dar = "boa tarde!"
 		else: dar = "boa noite!"
 		return dar
