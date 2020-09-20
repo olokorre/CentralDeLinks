3f1725f9fca3e8c1787681e35c8accc14e697df1
olokorre <andreiruansteffens@gmail.com>
2020-09-19 22:31:15 -0300

    criação de usuarios

--- main.py
+++ main.py
@@ -18,6 +18,13 @@ class BancoDeDados(object):
 		self.mycursor = self.mydb.cursor()
 		self.mycursor.execute("use Links")
 
+	def basic_request(self, user):
+		self.mycursor.execute('select * from users')
+		for i in self.mycursor:
+			print(i[0], user)
+			if user == i[0]: table = i[1]
+		return table
+
 	def user_check(self, nick):
 		self.mycursor.execute('select * from users')
 		list_of_users = []
@@ -25,19 +32,23 @@ class BancoDeDados(object):
 			list_of_users.append(i[0])
 		if nick in list_of_users: return True
 		return False
+	
+	def create_user(self, nick):
+		try:
+			table = 'table_' + nick
+			self.mycursor.execute('insert into users (nick, tables) value ("%s", "%s")' % (nick, table))
+			self.mycursor.execute('create table %s (id int(255) primary key auto_increment, nome varchar(255), link varchar(255))' % (table))
+			return True
+		except:
+			return False
 
 	def request(self, user):
-		self.mycursor.execute('select * from users')
-		for i in self.mycursor:
-			print(i[0], user)
-			if user == i[0]: table = i[1]
+		table = self.basic_request(user)
 		self.mycursor.execute('select * from %s' % (table))
 		return self.mycursor
 
-	def enviar(self, user, nome, link):
-		self.mycursor.execute('select * from users')
-		for i in self.mycursor:
-			if user == i[0]: table = i[1]
+	def send_data(self, user, nome, link):
+		table = self.basic_request(user)
 		self.mycursor.execute('insert into %s (nome, link) value ("%s", "%s")' % (table, nome, link))
 		self.mydb.commit()
 
@@ -57,26 +68,52 @@ def index():
 	nome = []
 	link = []
 	if request.method == 'GET':
-		if user == "Entrar": return redirect('/user_mananger')
+		if user == "Entrar": return redirect('/login')
 		linksDis = BD.request(user)
 		for i in linksDis: # agrupar os nomes e os links em suas respectivas listas
 			nome.append(i[1])
 			link.append(i[2])
-		return render_template('index.html', dar = saudacao.dar(), nome = nome, link = link, tam = len(nome), user = user) # renderiza e entrega o templete ao cliente
+		return render_template('index.html', 
+			dar = saudacao.dar(), 
+			nome = nome, 
+			link = link, 
+			tam = len(nome), 
+			user = user) # renderiza e entrega o templete ao cliente
 	if request.method == 'POST': # encarregado de receber os novos links
-		BD.enviar(user, request.form['nome'], request.form['link'])
+		BD.send_data(user, request.form['nome'], request.form['link'])
 		return redirect('/')
 
-@app.route('/user_mananger', methods = ('GET', 'POST'))
-def user_mananger():
+@app.route('/login', methods = ('GET', 'POST'))
+def login():
 	global user
 	if request.method == 'GET': return render_template('login.html', dar = saudacao.dar())
 	elif request.method == 'POST':
 		nick = request.form['user']
 		if BD.user_check(nick): user = nick
-		else: user = "Entrar"
+		else:
+			user = "Entrar"
+			return render_template('error.html', 
+				erro = 'Usuario não existe...', 
+				url = '/create', 
+				action = 'Registrar-se', 
+				dar = saudacao.dar())
 		return redirect('/')
-	return '404'
+	return redirect('/create')
+
+@app.route('/create', methods = ('GET', 'POST'))
+def create():
+	global user
+	if request.method == 'GET': return render_template('create.html', dar = saudacao.dar())
+	elif request.method == 'POST':
+		nick = request.form['user']
+		if BD.create_user(nick): 
+			user = nick
+			return redirect('/')
+		else: return render_template('error.html', 
+			erro = 'Esse usuario já existe...', 
+			url = '/login', 
+			action = "Entrar", 
+			dar = saudacao.dar())
 
 # rotas auxiliares
 @app.route('/js/<path:path>')
