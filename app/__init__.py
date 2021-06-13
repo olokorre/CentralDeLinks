84633c2c40f44dc3d544f93d4445239981ccee6d
Andrei Ruan Steffens <andreiruansteffens@gmail.com>
2021-06-13 12:22:23 -0300

    melhorar a seguraça da autenticacao

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,9 @@ from flask import Flask
 import db
 from decouple import config
 
+
 app = Flask(__name__)
+app.secret_key = config('SECRET_KEY')
 BD = db.BancoDeDados(config('USER_DB'), config('PASSWD_DB'))
 
 from .routes import aux, index, users
