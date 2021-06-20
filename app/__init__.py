69778200f40c5b5008fdd4eaf84d8e1b6054695b
Andrei Ruan Steffens <andreiruansteffens@gmail.com>
2021-06-20 02:54:26 +0000

    deu ruim

--- app/__init__.py
+++ app/__init__.py
@@ -2,18 +2,6 @@ from flask import Flask
 import db
 from decouple import config
 
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
-
 app = Flask(__name__)
 app.secret_key = config('SECRET_KEY')
 BD = db.BancoDeDados(config('USER_DB'), config('PASSWD_DB'))
